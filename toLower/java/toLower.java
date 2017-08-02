f7c61883513c47d40f6ebc574eb325d3db9cdd25
Dewan Ahmed <dewana@ca.ibm.com>
2017-08-02 10:41:45 -0500

    Fix for

--- toLower/java/toLower.java
+++ toLower/java/toLower.java
@@ -6,7 +6,8 @@ public class toLower {
       Scanner user_input = new Scanner( System.in );
       String input;
       System.out.print("Enter a text to turn into lowercase: ");
-      input = user_input.next( );
+      // Use user_input.nextLine() instead of user_input.next() to get all words. Else texts after space will be neglected
+      input = user_input.nextLine();
       System.out.println("Your input is converted to lowercase: "+ input.toLowerCase());
    }
 }
