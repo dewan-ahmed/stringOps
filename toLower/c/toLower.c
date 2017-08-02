9761418764c00d7ab5ade39477851e3ec42d71ad
Dewan Ahmed <dewana@ca.ibm.com>
2017-08-02 11:33:43 -0500

    Fix for https://github.com/dewan-ahmed/stringOps/issues/3

--- toLower/c/toLower.c
+++ toLower/c/toLower.c
@@ -1,19 +1,31 @@
 //Given a string, this program converts it into lowercase
+//Using buffer here as typical use of scanf ignores text after whitespace
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(void)
 {
-   char string[100];
-   int i = 0;
+    int i = 0;
+    char *buffer;
+    size_t bufsize = 32;
+    size_t string;
+
+    buffer = (char *)malloc(bufsize * sizeof(char));
+    if( buffer == NULL)
+    {
+        perror("Unable to allocate buffer");
+        exit(1);
+    }
    printf("Enter any string: ");
-   scanf("%s", string);
-   printf("The input string is converted to lowercase: ");    
-   while(string[i] ) 
+   string = getline(&buffer,&bufsize,stdin);
+   printf("You typed: %s\n",buffer);    
+   printf("Converted to lowercase: ");
+   while(buffer[i]) 
    {
-      putchar(tolower(string[i]));
-      i++;
+       putchar(tolower(buffer[i]));
+       i++;
    }
    printf ("\n");
-    return 0;
+   return 0;
 }
